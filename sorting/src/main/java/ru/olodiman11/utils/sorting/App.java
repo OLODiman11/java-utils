b49a8b6635bbf5d677f4b5e4d3f8205a144e42b6
OLODiman11 <OLODiman11@mail.ru>
2024-07-26 21:31:42 +0300

    Initial commit

---
+++ sorting/src/main/java/ru/olodiman11/utils/sorting/App.java
@@ -0,0 +1,13 @@
+package ru.olodiman11.utils.sorting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
